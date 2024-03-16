15d7e54989e28c7f94c749fadf138ac74175b1f9
Sunny <sobhit230@gmail.com>
2024-03-17 00:53:57 +0530

    Change

---
+++ Currency-Converter/script.js
@@ -0,0 +1,15 @@
+
+
+
+const dropdowns = document.querySelectorAll('.dropdown select');
+
+
+
+for(let select of dropdowns){
+    for(let currCode in countryList){
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        select.append(newOption);
+    }
+}
