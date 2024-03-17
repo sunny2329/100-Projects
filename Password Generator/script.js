d8b3bb4343749aa077e92e874fe39ee9cb7b1934
Sunny <sobhit230@gmail.com>
2024-03-17 18:06:00 +0530

    Password Generator

---
+++ Password Generator/script.js
@@ -0,0 +1,10 @@
+const display = document.getElementById('disp');
+const button = document.querySelector('.generate');
+
+const length = document.querySelector('#range');
+const numbers = document.querySelector('#numbers');
+const chars = document.querySelector('#chars');
+
+const lengthText = document.querySelector('#length');
+
+
