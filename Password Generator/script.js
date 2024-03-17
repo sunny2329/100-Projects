fa2c2a6bf5c94ec85493d7d1db7a5125a31bae5e
Sunny <sobhit230@gmail.com>
2024-03-17 18:09:53 +0530

    Password Generator

--- Password Generator/script.js
+++ Password Generator/script.js
@@ -1,10 +1,30 @@
-const display = document.getElementById('disp');
-const button = document.querySelector('.generate');
+function randPassword(){
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&{}[]";
+    let pass = "";
 
-const length = document.querySelector('#range');
-const numbers = document.querySelector('#numbers');
-const chars = document.querySelector('#chars');
+    let i = 0;
+    while(i<12){
+        let ind = Math.floor(Math.random()*str.length);
+        let char = str.charAt(ind);
+        pass += char;
+        i++; 
+    }
+    return pass;
+}
 
-const lengthText = document.querySelector('#length');
+const generateBtn = document.querySelector('button');
+const inputBox = document.querySelector('input');
+const copyBtn = document.querySelector('i');
 
 
+copyBtn.addEventListener('click',()=>{
+    inputBox.select();
+    window.navigator.clipboard.writeText(inputBox.value);
+
+})
+
+
+generateBtn.addEventListener('click',()=>{
+    let password = randPassword();
+    inputBox.value = password;
+})
